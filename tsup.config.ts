bcd64fbfb42f29836683eb1a2a65cfc69b99c4a3
Maël Nison <nison.mael@gmail.com>
2025-10-06 16:38:28 +0200

    Adds tsup for TS packages

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'tsup';
+
+// eslint-disable-next-line arca/no-default-export
+export default defineConfig([{
+  entry: [`packages/clipanion-tools/index.ts`],
+  outDir: `packages/clipanion-tools/dist`,
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+}, {
+  entry: [`packages/clipanion-expressive-code/index.ts`],
+  outDir: `packages/clipanion-expressive-code/dist`,
+  dts: true,
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+}]);
