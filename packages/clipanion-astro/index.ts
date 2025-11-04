64234744a04a72f9c0160a5e3444b6bf49669376
Maël Nison <nison.mael@gmail.com>
2025-11-04 23:02:12 +0100

    Adds watcher support to the astro plugin

--- packages/clipanion-astro/index.ts
+++ packages/clipanion-astro/index.ts
@@ -1,5 +1,4 @@
 import { ClipanionBinary, CommandSpec } from '@clipanion/tools';
-import { DataEntry as AstroDataEntry } from 'astro/content/config';
 import type { Loader } from 'astro/loaders';
 
 export type BaseData = {
@@ -17,52 +16,65 @@ export type CliOptions<T extends Record<string, unknown>> = {
   id?: string;
   name: string;
   path: string;
+  filter?: (entry: DataEntry<BaseData>) => boolean;
   entry?: (entry: DataEntry<BaseData>) => DataEntry<T> | Promise<DataEntry<T>>;
   body?: (data: DataEntry<T>) => string;
 };
 
 export function clipanionLoaders<T extends Record<string, unknown> = BaseData>(opts: CliOptions<T>) {
   const binary = new ClipanionBinary(opts.path);
-  const commandSpecs = binary.commands();
 
   return {
-    commands: createCommandLoader<T>(opts, commandSpecs),
+    commands: createCommandLoader<T>(opts, binary),
   };
 };
 
-function createCommandLoader<T extends Record<string, unknown> = BaseData>({ id, name, entry: entryFn, body: bodyFn }: CliOptions<T>, commandSpecs: Promise<CommandSpec[]>): Loader {
+function createCommandLoader<T extends Record<string, unknown> = BaseData>({ id, name, filter: filterFn, entry: entryFn, body: bodyFn }: CliOptions<T>, binary: ClipanionBinary): Loader {
   return {
     name: `@clipanion/astro`,
 
     load: async ({ renderMarkdown, store }) => {
-      store.clear();
-      for (const commandSpec of await commandSpecs) {
-        const baseData: BaseData = {
-          binaryName: name,
-          commandSpec,
-        };
+      const sync = async () => {
+        store.clear();
 
-        const baseEntry = {
-          id: [id ?? name, ...commandSpec.primaryPath].join(`/`),
-          data: baseData,
-        };
+        for (const commandSpec of await binary.commands()) {
+          const baseData: BaseData = {
+            binaryName: name,
+            commandSpec,
+          };
 
-        const entry = entryFn
-          ? await entryFn(baseEntry)
-          : baseEntry as any as DataEntry<T>;
+          const baseEntry = {
+            id: [id ?? name, ...commandSpec.primaryPath].join(`/`),
+            data: baseData,
+          };
 
-        const body = bodyFn?.(entry);
+          if (filterFn?.(baseEntry) === false) {
+            continue;
+          }
 
-        const rendered = body
-          ? await renderMarkdown(body)
-          : undefined;
+          const entry = entryFn
+            ? await entryFn(baseEntry)
+            : baseEntry as any as DataEntry<T>;
 
-        store.set({
-          ...entry,
-          body,
-          rendered,
-        });
-      }
+          const body = bodyFn?.(entry);
+
+          const rendered = body
+            ? await renderMarkdown(body)
+            : undefined;
+
+          store.set({
+            ...entry,
+            body,
+            rendered,
+          });
+        }
+      };
+
+      binary.onUpdate(async () => {
+        await sync();
+      });
+
+      await sync();
     },
   };
 }
